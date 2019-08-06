989e9c607d61a245ee2c6d67ec8a2e801f799918
Omar Gonzalez <gonzalezoe82@gmail.com>
2019-08-06 16:34:42 +0900

    displaying one user

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 const card = document.createElement("div");
 card.classList.add("card");
 document.getElementById("gallery").appendChild(card);
-
+const imgContainer = document.createElement("div");
+imgContainer.classList.add("card-img-container");
+card.appendChild(imgContainer)
+const url = 'https://randomuser.me/api/?results=12';
 
 function createElement(element) {
     return document.createElement(element);
@@ -11,37 +14,40 @@ function append(parent, element) {
     return parent.appendChild(element);
 }
 
-// function fetchData(url) {
-//     return fetch(url)
-//             .then(res => res.json())
-//             .then(data => console.log( data.results ))
-//             .catch(error => console.log('Looks like there was a problem', error))
-// }
 
 
-
-
-fetch('https://randomuser.me/api/?results=12')
-    .then((resp) => resp.json())
-    .then(function(data) {
-        let people = data.results;
-        return people.map(function(person) {
-        let divImg = createElement("div");
-        divImg.classList.add("card-img-container");
-        let infoDiv = createElement("div");
-        infoDiv.classList.add("card-info-container");
-        append(card, divImg);
-        append(card, infoDiv);
-        divImg.innerHTML = `<img class="card-img" src=${person.picture.thumbnail} alt="profile picture">`
-        infoDiv.innerHTML = `
-        <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
-        <p class="card-text">${person.email}</p>
-        <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
-        `
-
-        })
+fetch(url)
+    .then(response => response.json())
+    .then(function (data) {
+        const people = data.results
+        generateImage(people)
+        generateHTML(people)
     })
-    .catch(function (error){
-        console.log(error);
+    // .then( data => generateImage(data))
+
+
+
+function generateImage(data) {
+    const pictures = data.map( data => {
+        imgContainer.innerHTML =  `<img class="card-img" src=${data.picture.medium} alt="profile picture">`;
     })
+     return pictures
+    // console.log(data)
     
+                    
+}   
+function generateHTML(data) { 
+    
+ 
+    const infoDiv = createElement("div");
+    infoDiv.classList.add("card-info-container");
+    append(card, infoDiv);
+    const peopleInfo = data.map( data => {
+    infoDiv.innerHTML = `
+        <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
+        <p class="card-text">${data.email}</p>
+        <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
+        `;
+    })
+    return peopleInfo
+}
