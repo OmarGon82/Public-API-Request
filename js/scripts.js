03f6bb83f4b6b63b14bdf4235570209ece4ebb35
Omar Gonzalez <gonzalezoe82@gmail.com>
2019-08-06 21:57:31 +0900

    generateHTML function is working. 12 ppl show up

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,28 @@
 const card = document.createElement("div");
 card.classList.add("card");
 document.getElementById("gallery").appendChild(card);
-const imgContainer = document.createElement("div");
-imgContainer.classList.add("card-img-container");
-card.appendChild(imgContainer)
-const url = 'https://randomuser.me/api/?results=12';
-
-function createElement(element) {
-    return document.createElement(element);
-}
-
-function append(parent, element) {
-    return parent.appendChild(element);
-}
-
+const url = 'https://randomuser.me/api/?results=11';
 
 
 fetch(url)
     .then(response => response.json())
     .then(function (data) {
         const people = data.results
-        generateImage(people)
         generateHTML(people)
     })
-    // .then( data => generateImage(data))
 
 
-
-function generateImage(data) {
-    const pictures = data.map( data => {
-        imgContainer.innerHTML =  `<img class="card-img" src=${data.picture.medium} alt="profile picture">`;
-    })
-     return pictures
-    // console.log(data)
-    
-                    
-}   
-function generateHTML(data) { 
-    
  
-    const infoDiv = createElement("div");
-    infoDiv.classList.add("card-info-container");
-    append(card, infoDiv);
-    const peopleInfo = data.map( data => {
-    infoDiv.innerHTML = `
-        <h3 id="name" class="card-name cap">${data.name.first} ${data.name.last}</h3>
-        <p class="card-text">${data.email}</p>
-        <p class="card-text cap">${data.location.city}, ${data.location.state}</p>
+function generateHTML(data) { 
+    data.forEach(function (person) {
+        card.innerHTML = `
+        <div class="card-info-container">
+        <img class="card-img" src=${person.picture.medium} alt="profile picture">
+        <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+        <p class="card-text cap">${person.email}</p>
+        <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+        </div>
         `;
+        document.getElementById("gallery").appendChild(card.cloneNode(true))
     })
-    return peopleInfo
 }
